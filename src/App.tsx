758d1e0b520a8aa66cb15a66323f171d114335c9
marioxaxa <marioxaxa0@gmail.com>
2023-09-19 16:27:35 -0300

    Bug de configurações

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
-    <>
-      <h1>ggg</h1>
-    </>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload ia</h1>
+
+        
+        <div className="flex items-center gap-3">
+          <span>n sei</span>
+          <Button>Github</Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
